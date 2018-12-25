5e5837e072e1ae17a7d3e496378f44c664b0bef7
GainiZh <gaini.zhulamanova@gmail.com>
2018-12-25 15:07:59 +0100

    refactor: service worker

--- sw.js
+++ sw.js
@@ -1,58 +1,59 @@
-const cacheFiles = [
-  '/',
-  '/index.html',
-  '/restaurant.html',
-  '/css/styles.css',
-    '/js/dbhelper.js',
-    '/js/main.js',
-    '/js/restaurant_info.js',
-    '/data/restaurants.json',
-    '/img/1.jpg',
-    '/img/2.jpg',
-    '/img/3.jpg',
-    '/img/4.jpg',
-    '/img/5.jpg',
-    '/img/6.jpg',
-    '/img/7.jpg',
-    '/img/8.jpg',
-    '/img/9.jpg',
-    '/img/10.jpg'
-];
+let staticCache = 'restaurant-static';
 
 // Installation
 
 self.addEventListener('install', function(e) {
   e.waitUntil(
-    caches.open('v1').then(function(cache) {
-      return cache.addAll(cacheFiles);
+    caches.open(staticCache).then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/restaurant.html',
+        '/css/styles.css',
+          '/js/dbhelper.js',
+          '/js/main.js',
+          '/js/restaurant_info.js',
+          '/data/restaurants.json',
+          '/img/1.jpg',
+          '/img/2.jpg',
+          '/img/3.jpg',
+          '/img/4.jpg',
+          '/img/5.jpg',
+          '/img/6.jpg',
+          '/img/7.jpg',
+          '/img/8.jpg',
+          '/img/9.jpg',
+          '/img/10.jpg'
+      ]);
     })
   );
 });
 
-// check if the service worker registered
-// console.log('Service Worker: Registered');
+// Activate
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys()
+    .then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('restaurant-') &&
+                  cacheName != staticCache;
+        }).map(function(cacheName) {
+              return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+})
 
 // Fetching
 
 self.addEventListener('fetch', function(e) {
   e.respondWith(
-    caches.match(e.request).then(function(response) {
-      if (response) {
-        return response;
-      }
-      else {
-        return fetch(e.request);
-        .then(function(response) {
-          const clonedResponse = response.clone();
-          caches.open('v1').then(function(cache) {
-            cache.put(e.request, clonedResponse);
-          })
-          return response;
-        })
-        .catch(function(err) {
-          console.error(err);
-        });
-      }
+    caches.match(e.request)
+    .then(function(response) {
+      return response || fetch(e.request);
     })
   );
 });
